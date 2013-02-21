fe7448150b8fe42aca2082fb6ba96c3905c163b5
Ben Webb <bjwebb67@googlemail.com>
2013-02-21 16:27:27 +0000

    Tweaks for django 1.4 etc

--- import.py
+++ import.py
@@ -20,7 +20,9 @@ cat = {
     51: 8,
     52: 9,
     54: 10,
-    55: 11	
+    55: 11,
+    56: 12,
+    57: 13
 }
 
 weevils = {}
@@ -29,28 +31,29 @@ for i in range(1,14):
     weevils[i] = weevil
     weevil.save()
 
-conn = MySQLdb.connect(host='localhost', user='root', passwd=getpass.getpass(), db='weevil') 
+conn = MySQLdb.connect(host='127.0.0.1', user='weevil', passwd=getpass.getpass(), db='weevil') 
 c = conn.cursor()
-c.execute('SELECT catid, alias, title, introtext FROM jos_content WHERE sectionid=6')
+c.execute('SELECT catid, alias, title, introtext FROM jos_content WHERE sectionid=6 AND state=1')
 for row in c:
     name = row[2].decode('latin-1')
     text = row[3].decode('latin-1')
     contributor = Contributor(name=name, slug=row[1], text=text)
     contributor.save()
 
-c.execute('SELECT catid, alias, title, introtext, created_by_alias FROM jos_content WHERE sectionid=9')
+c.execute('SELECT catid, alias, title, introtext, created_by_alias FROM jos_content WHERE sectionid=9 AND state=1')
 # TODO Deal with multiple authors
 created_re = re.compile('{ga=([^,&]*)(,?([^&,]*)&t)?}')
 for row in c:
     if row[0] in cat:
         try:
             text = row[3].decode('latin-1')
+            title = row[2].decode('latin-1')
         except UnicodeDecodeError:
             print row[3]
             import sys
             sys.exit(0)
         article = Article(
-            title=row[2],
+            title=title,
             slug=row[1],
             text=text,
             magazine=weevils[cat[int(row[0])]]
