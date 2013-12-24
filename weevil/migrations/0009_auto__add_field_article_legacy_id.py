de627e94311d6c3d69fbed1c318fac56d3855194
Ben Webb <bjwebb67@googlemail.com>
2013-12-24 21:09:14 +0000

    Add legacy id migration

---
+++ weevil/migrations/0009_auto__add_field_article_legacy_id.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Article.legacy_id'
+        db.add_column(u'weevil_article', 'legacy_id',
+                      self.gf('django.db.models.fields.IntegerField')(null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Article.legacy_id'
+        db.delete_column(u'weevil_article', 'legacy_id')
+
+
+    models = {
+        u'weevil.article': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Article'},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'articles_written'", 'null': 'True', 'to': u"orm['weevil.Contributor']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'illustrator': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'articles_illustrated'", 'null': 'True', 'to': u"orm['weevil.Contributor']"}),
+            'legacy_id': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'magazine': ('adminsortable.fields.SortableForeignKey', [], {'to': u"orm['weevil.Magazine']"}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'})
+        },
+        u'weevil.committee': {
+            'Meta': {'object_name': 'Committee'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'year': ('django.db.models.fields.IntegerField', [], {})
+        },
+        u'weevil.contributor': {
+            'Meta': {'object_name': 'Contributor'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"})
+        },
+        u'weevil.magazine': {
+            'Meta': {'ordering': "['order']", 'object_name': 'Magazine'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'issue_number': ('django.db.models.fields.IntegerField', [], {}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1', 'db_index': 'True'}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"})
+        },
+        u'weevil.news': {
+            'Meta': {'object_name': 'News'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
+            'text': ('django.db.models.fields.TextField', [], {'default': "''"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+            'updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['weevil']
